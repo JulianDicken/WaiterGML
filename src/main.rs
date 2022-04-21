511b78754feeef73f6fdd82f9071cc482639fa29
Julian Dicken <julian@dicken.de>
2022-04-21 13:38:14 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,25 @@ use std::io::prelude::*;
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let note_dir = format!("{}{}{}", &args[1], "\\", "notes\\");
+    for path_to_note_folder in fs::read_dir(&note_dir)
+        .expect("unable to read notes directory") {
+        for path_to_note in fs::read_dir(path_to_note_folder?.path())
+            .expect("unable to read note directory") {
+                let path = path_to_note?.path();
+                let ex = path.extension().unwrap().to_str();
+                //if path.is_dir() { continue; }
 
-    for path in fs::read_dir(&note_dir).expect("unable to read note directory") {
-        let note = path.expect("unable to read note");
-        let note_path = format!("{:?}.txt", note.path());
-        //let file = fs::File::open(note.path())?;
-        print!("{:?}", note_path);
-        //let mut buf_reader = io::BufReader::new(file);
-        //let mut content = String::new();
-        //buf_reader.read_to_string(&mut content).expect("error reading note contents");
-        //println!("{}", content);
+            let file = fs::File::open(&path).expect("unable to open file");
+            let buf_reader = io::BufReader::new(file);
+            match ex {
+                Some("txt") => { }
+                _ => continue
+            };
+            for (index, line) in buf_reader.lines().enumerate() {
+                let line = line.unwrap();
+                println!("{}. {}", index + 1, line);
+            }
+        }
     }
     Ok(())
 } 
