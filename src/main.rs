76840b99d82e0ecd6c4ca2c00408a00a69026fe2
Julian Dicken <julian@dicken.de>
2022-04-21 14:54:21 +0200

    uhhhhhhhhhhhhhh

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,32 @@ use std::env;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
+use regex::Regex;
+
+fn handle_import(statement: &str) {
+    let is_github_import = Regex::new(r"[a-zA-Z0-9]*/[a-zA-Z0-9]+").unwrap();
+    let is_url_import = Regex::new(r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)").unwrap();
+    //let is_local_import = Regex::new(r"").unwrap(); we do this later
+    if is_github_import.is_match(statement) {
+        handle_github_import(statement);
+    } else if is_url_import.is_match(statement) {
+        handle_url_import(statement);
+    } else {
+        println!("invalid import: {}", statement)
+    }
+}
+
+fn handle_github_import(statement: &str) {
+    println!("github import: {}", statement)
+}
+fn handle_url_import(statement: &str) {
+    println!("url import: {}", statement)
+}
 
 fn main() -> std::io::Result<()> {
+    const STATEMENT_PREFIX: &str = "#";
+    const STATEMENT_IMPORT: &str = "import";
+
     let args: Vec<String> = env::args().collect();
     let note_dir = format!("{}{}{}", &args[1], "\\", "notes\\");
     for path_to_note_folder in fs::read_dir(&note_dir)
@@ -12,18 +36,26 @@ fn main() -> std::io::Result<()> {
             .expect("unable to read note directory") {
                 let path = path_to_note?.path();
                 let ex = path.extension().unwrap().to_str();
-                //if path.is_dir() { continue; }
+                if ex != Some("txt") { continue }
 
             let file = fs::File::open(&path).expect("unable to open file");
             let buf_reader = io::BufReader::new(file);
-            match ex {
-                Some("txt") => { }
-                _ => continue
-            };
-            for (index, line) in buf_reader.lines().enumerate() {
-                let line = line.unwrap();
-                println!("{}. {}", index + 1, line);
+
+            for (_, line) in buf_reader.lines().enumerate() {
+                let unwrapped_line = &line.unwrap();
+                match &unwrapped_line[0..1] {
+                    STATEMENT_PREFIX => { 
+                        match &unwrapped_line[1..7] {
+                            STATEMENT_IMPORT => { 
+                                handle_import(&unwrapped_line)
+                            }
+                            _ => { }
+                        }
+                    },
+                    _ => {}
+                }
             }
+            
         }
     }
     Ok(())
