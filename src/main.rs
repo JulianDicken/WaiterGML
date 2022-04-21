34f622278c6758e7def2a9a902cc293ac19543b1
Julian Dicken <julian@dicken.de>
2022-04-21 12:31:11 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::env;
 use std::fs;
+use std::io;
+use std::io::prelude::*;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    let dir = &args[1];
-    let paths = fs::read_dir(&dir).unwrap();
+    let note_dir = format!("{}{}{}", &args[1], "\\", "notes\\");
 
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
+    for path in fs::read_dir(&note_dir).expect("unable to read note directory") {
+        let note = path.expect("unable to read note");
+        let file = fs::File::open(note.path())?;
+        let mut buf_reader = io::BufReader::new(file);
+        let mut content = String::new();
+        buf_reader.read_to_string(&mut content).expect("error reading note contents");
+        println!("{}", content);
     }
-}
+    Ok(())
+} 
